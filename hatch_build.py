7b50303033e7e1dd9776893003919656c6f743d8
Christophe Demko <chdemko@gmail.com>
2025-02-16 14:04:52 +0100

    Cleaning files

--- hatch_build.py
+++ hatch_build.py
@@ -1,6 +1,7 @@
 """Script for th generation of compiled modules using Cython."""
 
 import shutil
+from collections.abc import Iterable
 from pathlib import Path
 from typing import Any
 
@@ -14,6 +15,12 @@ class CustomBuildHook(BuildHookInterface):
     """
     Custom build hook.
     """
+
+    @classmethod
+    def _get_directories(cls) -> Iterable[Path]:
+        return [
+            Path("src", "arithmetic"),
+        ]
 
     def initialize(
         self,
@@ -33,20 +40,27 @@ class CustomBuildHook(BuildHookInterface):
         build_data["pure_python"] = False
         build_data["infer_tag"] = True
 
+        # Setting directories
+        directories = self._get_directories()
+
+        # Cleanup project
+        self._clean()
+
+        # Getting cython files
         extensions = [
-            Extension(
-                "*",
-                ["src/arithmetic/*.pyx"],
-            ),
+            Extension("*", [Path(directory, "*.pyx")]) for directory in directories
         ]
 
+        # Cythonize modules
         ext_modules = cythonize(
             extensions,
             compiler_directives={"binding": True, "language_level": 3},
         )
 
+        # Get a distribution
         distribution = Distribution({"ext_modules": ext_modules})
 
+        # Build the distribution
         cmd = build_ext(distribution)
         cmd.ensure_finalized()
         cmd.run()
@@ -57,3 +71,32 @@ class CustomBuildHook(BuildHookInterface):
             relative_extension = Path("src", output_path.relative_to(cmd.build_lib))
             shutil.copyfile(output, relative_extension)
             relative_extension.chmod(output_path.stat().st_mode)
+
+    def _clean(self, *, complete: bool = False) -> None:
+        """
+        Clean up.
+
+        Parameters
+        ----------
+        complete
+            Remove also .c files
+
+        """
+        for directory in self._get_directories():
+            for file in directory.glob("*.pyx"):
+                if complete:
+                    file.with_suffix(".c").unlink(missing_ok=True)
+                for so_file in directory.glob(f"{file.stem}*.so"):
+                    so_file.unlink()
+
+    def clean(self, versions: Iterable[str]) -> None:  # noqa: ARG002
+        """
+        Clean up.
+
+        Parameters
+        ----------
+        versions
+            An iterable of string
+
+        """
+        self._clean(complete=True)
